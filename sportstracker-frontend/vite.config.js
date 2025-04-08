c3cf6f99444a13f15b329a03a01a945de3703ace
a.krueger <a.krueger@if-talent.de>
2025-04-08 22:49:41 +0200

    test

--- sportstracker-frontend/vite.config.js
+++ sportstracker-frontend/vite.config.js
@@ -3,7 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    visualizer({ open: true })
+  ],
   server: {
     host: '0.0.0.0',  // Binde Vite an alle IP-Adressen
     port: 3000        // Der Port bleibt 5173, falls du diesen nicht ändern möchtest
