ed641d88306d5d91fe7fc05c5522208e0b006b3a
a.krueger <a.krueger@if-talent.de>
2025-04-08 07:29:54 +0200

    DOcker DEV WOrking

--- sportstracker-frontend/vite.config.js
+++ sportstracker-frontend/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',  // Binde Vite an alle IP-Adressen
+    port: 3000        // Der Port bleibt 5173, falls du diesen nicht ändern möchtest
+  }
 })
